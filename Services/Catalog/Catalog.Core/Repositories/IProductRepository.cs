253a7dc860060c5c88eb492b1ac0e7679637df90
Glailton Costa <glailton.rc@gmail.com>
2026-02-28 16:46:23 -0300

    Fixing references

--- Services/Catalog/Catalog.Core/Repositories/IProductRepository.cs
+++ Services/Catalog/Catalog.Core/Repositories/IProductRepository.cs
@@ -1,10 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Catalog.Core.Entities;
+using Catalog.Core.Specifications;
 
 namespace Catalog.Core.Repositories
 {
-    internal interface IProductRepository
+    public interface IProductRepository
     {
+        Task<IEnumerable<Product>> GetAllAsync();
+        Task<Pagination<Product>> GetProducts(CatalogSpecParams specParams);
+        Task<IEnumerable<Product>> GetProductsByName(string name);
+        Task<IEnumerable<Product>> GetProductsByBrand(string name);
+        Task<Product> GetProduct(string productId);
+        Task<Product> CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string productId);
+        Task<ProductBrand> GetBrandByIdAsync(string brandId);
+        Task<ProductType> GetTypeByIdAsync(string typeId);
     }
 }
